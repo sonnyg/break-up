ac264c20bf689d9d4cbd01489cd151ec5d6d3b87
sonnyg <sonny@evagabond.com>
2016-10-18 09:25:16 -0700

    adding game paddle, mouse move events for paddle control

--- src/main.js
+++ src/main.js
@@ -14,8 +14,37 @@ window.onload = function onLoad() {
     log(`found canvas: ${size.width} by ${size.height}`);
 
     const gameBoard = createGameBoard(size);
+    const paddle = createPaddle(gameBoard);
+
+    // move paddle to bottom, center of gameBoard
+    const paddleX = gameBoard.width / 2 - paddle.width / 2;
+    const paddleY = gameBoard.height - paddle.height - 2; // small bottom gap
+
+    paddle.moveTo(paddleX, paddleY);
 
     drawGameBoard(context, gameBoard);
+    drawPaddle(context, paddle);
+
+    // listen for mouse events on the document
+    document.onmousemove = function onMouseMove(event) {
+      // since we want the user to be able to control the paddle from anywhere
+      // in the document, we have to do some math to see if the mouse pointer
+      // "lines" up, vertically, with the canvas. Since the paddle can only move
+      // left and right, we only care about the mouse's x coordinate.
+
+      // start by getting the x coordinate relative to the document
+      const mouseX = event.clientX;
+
+      // now find out what the mouse's x coordinate is 'inside' the canvas
+      const canvasX = mouseX - canvas.offsetLeft;
+
+      // log(`mouseX: ${mouseX}, canvasX: ${canvasX}`);
+      // now update the paddle
+      paddle.moveTo(canvasX, paddle.y);
+
+      // redraw the paddle to see it's new position
+      drawPaddle(context, paddle);
+    }
 }
 
 function log(message) {
@@ -27,6 +56,16 @@ function drawGameBoard(context, gameBoard) {
 
     context.fillStyle = gameBoard.color;
     context.fillRect(0, 0, gameBoard.width, gameBoard.height);
+
+    context.restore();
+}
+
+function drawPaddle(context, paddle) {
+    context.save();
+
+    context.fillStyle = paddle.color;
+    context.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+
     context.restore();
 }
 
@@ -35,5 +74,19 @@ function createGameBoard(size) {
     width: size.width,
     height: size.height,
     color: "silver"
+  };
+}
+
+function createPaddle(gameBoard) {
+  return {
+    x: 0,
+    y: 0,
+    width: 100,
+    height: 12,
+    color: "blue",
+    moveTo : function moveTo(dx, dy) {
+      this.x = dx;
+      this.y = dy;
+    }
   };
 }
